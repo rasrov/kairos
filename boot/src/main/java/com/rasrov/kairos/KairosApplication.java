e05dc6004078c156103567a20c0bb93769111916
rasul <rasulakhmeddibirov@gmail.com>
2025-01-20 13:30:24 +0100

    Api definition

--- boot/src/main/java/com/rasrov/kairos/KairosApplication.java
+++ boot/src/main/java/com/rasrov/kairos/KairosApplication.java
@@ -1,7 +1,9 @@
 package com.rasrov.kairos;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class KairosApplication {
     public static void main(String[] args) {
         SpringApplication.run(KairosApplication.class, args);
