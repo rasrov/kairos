02ab14d4c52800d294d8d3dd2f44c7ed22889812
rasul <rasulakhmeddibirov@gmail.com>
2025-01-20 11:40:32 +0100

    Initial boot module structure.

---
+++ boot/src/main/java/com/rasrov/kairos/KairosApplication.java
@@ -0,0 +1,9 @@
+package com.rasrov.kairos;
+
+import org.springframework.boot.SpringApplication;
+
+public class KairosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KairosApplication.class, args);
+    }
+}
