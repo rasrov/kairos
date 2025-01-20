3fc33177c9e80bbede48b88fdcbbaf767df8f47c
rasul <rasulakhmeddibirov@gmail.com>
2025-01-20 17:41:23 +0100

    Enable caching.

--- boot/src/main/java/com/rasrov/kairos/KairosApplication.java
+++ boot/src/main/java/com/rasrov/kairos/KairosApplication.java
@@ -2,8 +2,10 @@ package com.rasrov.kairos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class KairosApplication {
     public static void main(String[] args) {
         SpringApplication.run(KairosApplication.class, args);
